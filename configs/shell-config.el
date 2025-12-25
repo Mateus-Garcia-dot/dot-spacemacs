f1440898b41432f6bdb6115e22ed696cb6ce0adb
Mateus Medeiros Garcia <mateus@Mateuss-MacBook-Air.local>
2025-12-25 15:46:22 -0300

    fix emacs capturing ctrl c on vterm

--- configs/shell-config.el
+++ configs/shell-config.el
@@ -2,8 +2,7 @@
 
 (with-eval-after-load 'vterm
   (evil-set-initial-state 'vterm-mode 'emacs)
-  (setq vterm-keymap-exceptions '("C-x" "C-g" "C-h" "M-x" "M-o" "C-u"))
-  (define-key vterm-mode-map (kbd "C-c") #'vterm-send-C-c))
+  (evil-define-key 'emacs vterm-mode-map (kbd "C-c") #'vterm-send-C-c))
 
 (define-key vterm-mode-map [(control return)]   #'vterm-toggle-insert-cd)
 
