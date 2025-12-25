480c32f3ab4f52d2b2b2f9a397b0645db6d0031f
Mateus Medeiros Garcia <69606533+Mateus-Garcia-dot@users.noreply.github.com>
2025-12-25 18:31:41 -0300

    fix define key vterm

--- configs/shell-config.el
+++ configs/shell-config.el
@@ -9,9 +9,8 @@
   (evil-set-initial-state 'vterm-mode 'emacs)
   (evil-define-key 'emacs vterm-mode-map (kbd "s-v") #'my/vterm-paste)
   (evil-define-key 'emacs vterm-mode-map (kbd "s-c") #'vterm-copy-mode)
-  (evil-define-key 'emacs vterm-mode-map (kbd "C-c") #'vterm-send-C-c))
-
-(define-key vterm-mode-map [(control return)]   #'vterm-toggle-insert-cd)
+  (evil-define-key 'emacs vterm-mode-map (kbd "C-c") #'vterm-send-C-c)
+  (define-key vterm-mode-map [(control return)]   #'vterm-toggle-insert-cd))
 
 (spacemacs/set-leader-keys
   "'" 'vterm-toggle)
