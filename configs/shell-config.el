f028b2fb80bf9647cf679abbd24e9998322f2fd5
Mateus Medeiros Garcia <69606533+Mateus-Garcia-dot@users.noreply.github.com>
2025-12-25 16:28:38 -0300

    fix copy and paste

--- configs/shell-config.el
+++ configs/shell-config.el
@@ -1,7 +1,14 @@
 (setq vterm-timer-delay 0.01)
 
+(defun my/vterm-paste ()
+  "Paste from system clipboard into vterm."
+  (interactive)
+  (vterm-insert (simpleclip-get-contents)))
+
 (with-eval-after-load 'vterm
   (evil-set-initial-state 'vterm-mode 'emacs)
+  (evil-define-key 'emacs vterm-mode-map (kbd "s-v") #'my/vterm-paste)
+  (evil-define-key 'emacs vterm-mode-map (kbd "s-c") #'vterm-copy-mode)
   (evil-define-key 'emacs vterm-mode-map (kbd "C-c") #'vterm-send-C-c))
 
 (define-key vterm-mode-map [(control return)]   #'vterm-toggle-insert-cd)
