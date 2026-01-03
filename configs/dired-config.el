12a2f12ea95547f7a5c35f78b08281978eea7aec
Mateus Medeiros Garcia <69606533+Mateus-Garcia-dot@users.noreply.github.com>
2026-01-03 14:39:50 -0300

    fix dired order

--- configs/dired-config.el
+++ configs/dired-config.el
@@ -1,3 +1,7 @@
+(setq insert-directory-program "gls")
+
+(setq dired-listing-switches "-alhv --group-directories-first --dired")
+
 (with-eval-after-load 'dired
   (add-hook 'dired-mode-hook #'dired-hide-details-mode))
 
